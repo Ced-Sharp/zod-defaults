6bd7f4ebed081aedcdd56ba402e4a024055eb87a
CedSharp <cedrik@cedsharp.ca>
2024-04-21 13:10:38 -0500

    Configured typescript

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+    // Browser
+    {
+        input: 'src/index.ts',
+        output: {
+            name: 'zod-defaults',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+        ],
+    },
+
+    // NodeJS
+    {
+        input: 'src/index.ts',
+        output: [
+            { file: pkg.main, format: 'cjs' },
+            { file: pkg.module, format: 'es' },
+        ],
+        plugins: [typescript({ tsconfig: './tsconfig.json' })],
+    },
+]
